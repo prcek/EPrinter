7bb61fc8e7f83c87ed3b978f32e40c292eb4d3e4
Tomas Hluchan <tomas.hluchan@gmail.com>
2018-09-04 11:59:29 +0200

    pusher

--- src/renderer/index.js
+++ src/renderer/index.js
@@ -3,6 +3,21 @@ import { render } from 'react-dom';
 import promiseIpc from 'electron-promise-ipc';
 import App from './App';
 
+import Pusher from 'pusher-js';
+
+
+var pusher = new Pusher("579feb079d3f094d5ad1",{
+  cluster: 'eu',
+  forceTLS: true
+});
+
+
+var channel = pusher.subscribe("my-channel");
+
+channel.bind("my-event",function(data){
+  console.log("pusher data",data);
+})
+
 
 console.log("renderer.js","start");
 
