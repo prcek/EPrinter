f6e4d9c07c0a032be49ea719958c6f3a6938c4cd
Tomas Hluchan <tomas.hluchan@gmail.com>
2018-09-04 17:45:26 +0200

    persist store

--- src/renderer/index.js
+++ src/renderer/index.js
@@ -4,13 +4,24 @@ import { render } from 'react-dom';
 import App from './App';
 
 
-
+import { PersistGate } from 'redux-persist/integration/react'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
-
+import { persistStore, persistReducer } from 'redux-persist'
 import rootReducer from './reducers'
 
-const store = createStore(rootReducer,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+import createElectronStorage from "redux-persist-electron-storage";
+const persistConfig = {
+  key: 'root',
+  storage: createElectronStorage()
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+let store = createStore(persistedReducer,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+let persistor = persistStore(store)
+
+
 
 import Pusher from 'pusher-js';
 
@@ -34,6 +45,8 @@ console.log("renderer.js","start");
 
 render( 
     <Provider store={store}>
-      <App printers={[]} />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>,
 document.getElementById('app') );
